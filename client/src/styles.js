062f6a523319acd2b97824f13ebb7556ddc4d823
Ahmed Al-Sabounchi <a_m_96@live.com>
2022-08-13 09:41:52 -0400

    Made UI more mobile-friendly

--- client/src/styles.js
+++ client/src/styles.js
@@ -1,6 +1,6 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-export default makeStyles(() => ({
+export default makeStyles((theme) => ({
   appBar: {
     borderRadius: 15,
     margin: '30px 0',
@@ -17,4 +17,9 @@ export default makeStyles(() => ({
     marginTop: '5px',
     color: 'rgba(0,183,255, 1)'
   },
+  [theme.breakpoints.down('sm')]: {
+    mainContainer: {
+      flexDirection: 'column-reverse'
+    }
+  }
 }));
