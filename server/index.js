998dc9d7576605124a4fe551cfa68ac5b13318c6
Ahmed Al-Sabounchi <a_m_96@live.com>
2022-08-17 18:34:48 -0400

    Fixed Google sign-in + added JWT registration/login

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import mongoose from 'mongoose'
 import cors from 'cors'
 
 import postRoutes from './routes/posts.js'
+import userRoutes from './routes/users.js'
 
 const app = express()
 
@@ -14,9 +15,10 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors())
 
 app.use('/posts', postRoutes)
+app.use('/user', userRoutes)
 const PORT = process.env.PORT || 5000
 
-
+process.env.MONGO_URI
 mongoose.connect(process.env.MONGO_URI)
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error))
