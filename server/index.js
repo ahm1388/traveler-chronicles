14134cdef48a5f065cb808437b41ba7e65c00e8b
Ahmed Al-Sabounchi <a_m_96@live.com>
2022-08-20 07:08:44 -0400

    Preparing app for Heroku deployment

--- server/index.js
+++ server/index.js
@@ -18,6 +18,10 @@ app.use('/posts', postRoutes)
 app.use('/user', userRoutes)
 const PORT = process.env.PORT || 5000
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('../client/build'))
+}
+
 process.env.MONGO_URI
 mongoose.connect(process.env.MONGO_URI)
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
