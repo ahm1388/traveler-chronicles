e70fcaec5fb9f337cded9ea306aa08d2f4518b37
Ahmed Al-Sabounchi <a_m_96@live.com>
2022-08-20 07:40:29 -0400

    More changes for Heroku deployment

--- server/index.js
+++ server/index.js
@@ -22,7 +22,6 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static('../client/build'))
 }
 
-process.env.MONGO_URI
 mongoose.connect(process.env.MONGO_URI)
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error))
